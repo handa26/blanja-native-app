08557d022e7a07824e0bc493da7603168ae38655
Handa <anandamuhammadmtq@gmail.com>
2021-01-18 22:35:27 +0800

    Input form for seller

--- src/screens/Auth/SignUp.js
+++ src/screens/Auth/SignUp.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import {View, TextInput} from 'react-native';
 import {Button, Text, Content, Container} from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -7,7 +8,12 @@ import HeadlineText from '../../components/HeadlineText/HeadlineText';
 import styles from './styles';
 
 const SignUp = ({navigation}) => {
-  const [value, onChangeText] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [store, setStore] = useState('');
+  const [number, setNumber] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Container>
       <CustomHeader
@@ -16,25 +22,45 @@ const SignUp = ({navigation}) => {
       />
       <HeadlineText condition="Sign Up" />
       <Content style={styles.formWrapper}>
-        {/* <Text style={styles.head}>Sign Up</Text> */}
         <View style={styles.formBox}>
           <TextInput
-            onChangeText={(val) => onChangeText(val)}
+            onChangeText={(val) => setName(val)}
+            defaultValue={name}
             style={styles.inputBox}
           />
           <Text style={styles.text}>Name</Text>
         </View>
         <View style={styles.formBox}>
           <TextInput
-            onChangeText={(val) => onChangeText(val)}
+            onChangeText={(val) => setEmail(val)}
+            defaultValue={email}
             style={styles.inputBox}
           />
-          <Text onChangeText={(val) => onChangeText(val)} style={styles.text}>
-            Email
-          </Text>
+          <Text style={styles.text}>Email</Text>
         </View>
-        <View onChangeText={(val) => onChangeText(val)} style={styles.formBox}>
-          <TextInput secureTextEntry style={styles.inputBox} />
+        <View style={styles.formBox}>
+          <TextInput
+            onChangeText={(val) => setStore(val)}
+            defaultValue={store}
+            style={styles.inputBox}
+          />
+          <Text style={styles.text}>Store Name</Text>
+        </View>
+        <View style={styles.formBox}>
+          <TextInput
+            onChangeText={(val) => setNumber(val)}
+            defaultValue={number}
+            keyboardType="phone-pad"
+            style={styles.inputBox}
+          />
+          <Text style={styles.text}>Telephone</Text>
+        </View>
+        <View defaultValue={password} style={styles.formBox}>
+          <TextInput
+            secureTextEntry
+            onChangeText={(val) => setPassword(val)}
+            style={styles.inputBox}
+          />
           <Text style={styles.text}>Password</Text>
         </View>
         <View style={styles.textBox}>
