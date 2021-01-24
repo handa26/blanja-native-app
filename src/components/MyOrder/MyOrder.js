7c7fba4d11e44e275f1aa14b6503b5d06f41b2a9
Handa <anandamuhammadmtq@gmail.com>
2021-01-24 13:48:58 +0800

    Order history by user

--- src/components/MyOrder/MyOrder.js
+++ src/components/MyOrder/MyOrder.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useSelector} from 'react-redux';
 import {ScrollView, View} from 'react-native';
+import {Spinner} from 'native-base';
+
+import {API_URL_DEVELOPMENT} from '@env';
 import HeadlineText from '../HeadlineText/HeadlineText';
 import CustomHeader from '../CustomHeader/CustomHeader';
 import OrderCard from '../OrderCard/OrderCard';
 
 const MyOrder = ({navigation}) => {
+  const [orders, setOrders] = useState([]);
+  const userId = useSelector((state) => state.auth.id);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL_DEVELOPMENT}history/${userId}`)
+      .then(({data}) => {
+        setOrders(data.data);
+        // console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, [userId]);
+
+  // console.log(orders);
   return (
     <ScrollView>
       <CustomHeader
@@ -15,11 +34,19 @@ const MyOrder = ({navigation}) => {
       />
       <HeadlineText condition="My orders" />
       <View>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {orders &&
+          orders.map((order) => {
+            return (
+              <OrderCard
+                key={order.id}
+                order={order.invoice_id.slice(12)}
+                trackNum={order.invoice_id}
+                qty={order.qty}
+                total={order.price}
+              />
+            );
+          })}
+        {/* <OrderCard /> */}
       </View>
     </ScrollView>
   );
--- src/components/OrderCard/OrderCard.js
+++ src/components/OrderCard/OrderCard.js
@@ -3,19 +3,19 @@ import {View, Text} from 'react-native';
 
 import styles from './styles';
 
-const OrderCard = () => {
+const OrderCard = ({order, trackNum, qty, total}) => {
   return (
     <View>
       <View style={styles.cardWrapper}>
         <View style={{padding: 10, marginTop: 15}}>
           <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Text style={{fontWeight: 'bold'}}>Order №1947034</Text>
+            <Text style={{fontWeight: 'bold'}}>Order {order}</Text>
             <Text style={{color: 'gray'}}>05-12-2019</Text>
           </View>
-          <Text style={{color: 'gray'}}>Tracking number: IW3475453455</Text>
-          <Text style={{color: 'gray'}}>Quantity: 3</Text>
+          <Text style={{color: 'gray'}}>Tracking number: {trackNum}</Text>
+          <Text style={{color: 'gray'}}>Quantity: {qty}</Text>
           <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Text style={{color: 'gray'}}>Total Amount: Rp. 50.000</Text>
+            <Text style={{color: 'gray'}}>Total Amount: Rp. {total}</Text>
             <Text style={{color: 'green'}}>Delivered</Text>
           </View>
         </View>
