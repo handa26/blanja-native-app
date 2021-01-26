eeb728c7eb178d655fd9478186012ac51d260ed0
Handa <anandamuhammadmtq@gmail.com>
2021-01-26 23:51:47 +0800

    Styling chat message, add user socket

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client';
 import {useSelector} from 'react-redux';
-import {View, StyleSheet, TextInput} from 'react-native';
+import {View, StyleSheet, TextInput, ScrollView} from 'react-native';
 import {Button, Text, Content, Container} from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -26,7 +26,7 @@ const Chat = ({navigation, route}) => {
   const submitChatMessage = () => {
     // Prevent firing empty message
     if (state.length !== 0) {
-      socket.emit('message', {state});
+      socket.emit('message', {state, user_name});
       setState('');
     }
   };
@@ -38,13 +38,15 @@ const Chat = ({navigation, route}) => {
         name={route.params.productName}
         leftIcon="arrow-left"
       />
-      <View style={{backgroundColor: 'lightgrey'}}>
+      <View>
         {chat.length !== 0 &&
-          chat.map(({state}, index) => {
+          chat.map(({state, user_name}, index) => {
             return (
               <View key={index}>
-                <Text>{user_name}</Text>
-                <Text>{state}</Text>
+                <Text style={styles.bubbleUser}>{user_name}</Text>
+                <View style={styles.bubbleChat}>
+                  <Text style={styles.bubbleText}>{state}</Text>
+                </View>
               </View>
             );
           })}
@@ -59,10 +61,15 @@ const Chat = ({navigation, route}) => {
               value={state}
               style={styles.inputBox}
             />
+            <Button style={styles.btn} onPress={submitChatMessage}>
+              <Icon
+                name="send"
+                size={20}
+                style={{marginLeft: 7}}
+                color="white"
+              />
+            </Button>
           </View>
-          <Button block style={styles.btn} onPress={submitChatMessage}>
-            <Text>Send</Text>
-          </Button>
         </View>
       </View>
     </Container>
@@ -77,23 +84,39 @@ const styles = StyleSheet.create({
   },
   formBox: {
     position: 'relative',
-    marginVertical: 20,
-    marginHorizontal: 20,
+    marginVertical: 5,
+    marginHorizontal: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   inputBox: {
-    width: 390,
-    height: 64,
+    width: 320,
+    height: 40,
     borderWidth: 1,
-    borderRadius: 4,
+    borderRadius: 40,
     borderColor: '#f1f1f1',
     padding: 10,
   },
+  bubbleUser: {
+    marginHorizontal: 20,
+    fontWeight: 'bold',
+    marginBottom: 3,
+    marginTop: 6,
+  },
+  bubbleChat: {
+    backgroundColor: '#DB3022',
+    marginHorizontal: 20,
+    padding: 10,
+    borderRadius: 10,
+  },
+  bubbleText: {
+    color: 'white',
+  },
   btn: {
     backgroundColor: '#DB3022',
-  },
-  chatHead: {
-    fontSize: 24,
-    marginLeft: 20,
+    height: 40,
+    width: 40,
+    borderRadius: 50,
   },
   footer: {
     position: 'absolute',
